d748c6d021b21af115a646422245d4b91fc836f6
TamHuynh15123 <hminhtam15123@gmail.com>
2026-01-25 13:53:32 +0700

    css

--- HotelManagementMVC/HotelManagementMVC/Controllers/HomeController.cs
+++ HotelManagementMVC/HotelManagementMVC/Controllers/HomeController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
 
 namespace HotelManagementMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRoomService _roomService;
+        private readonly IRoomTypeService _roomTypeService;
+
+        public HomeController(IRoomService roomService, IRoomTypeService roomTypeService)
+        {
+            _roomService = roomService;
+            _roomTypeService = roomTypeService;
+        }
+
         public IActionResult Index()
         {
             ViewBag.SuccessMessage = TempData["SuccessMessage"];
+            
+            // Get featured rooms (first 6 available rooms with their images)
+            var allRooms = _roomService.GetAll()
+                .Where(r => r.Status == BusinessObjects.Enums.RoomStatus.Available)
+                .Take(6)
+                .ToList();
+            
+            // Get all room types for display
+            var roomTypes = _roomTypeService.GetAll();
+            
+            ViewBag.FeaturedRooms = allRooms;
+            ViewBag.RoomTypes = roomTypes;
+            
             return View();
         }
     }
